62622359a3e5dcdd3e4dd62e2901c97ce85699fd
WenyeZhou51 <121508255+WenyeZhou51@users.noreply.github.com>
2023-09-24 09:11:35 -0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import asyncio
 
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix = commands.when_mentioned_or('?'), intents = intents)
+bot = commands.Bot(command_prefix = commands.when_mentioned_or('!'), intents = intents)
 
 @bot.event
 async def on_ready():
@@ -12,13 +12,12 @@ async def on_ready():
 
 async def main():
 	try:
-		await bot.load_extension("cogs.rps")
 		await bot.load_extension("cogs.util")
 		print(f'Extension loaded!')
 	except Exception as e:
 		print(f'Failed to load extension cogs.')
 		print(str(e))
-	await bot.start('MTE1NTE1NDE4NjkyOTk2NzIxNQ.G8niwM.GbwuDf8GYoCwbGi_ux9_RCiaocioUuPXEjUhJY')
+	await bot.start('MTE1NTE1NjI3OTIyMDc3Mjg2NA.GyAKuk.leEc2O0RI1si-Ci6CxDYJFcj-ZOkQ65xAD_jGw')
 
 asyncio.run(main())
 
